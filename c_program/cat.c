2dc724e1ec87ebf38e31782441932f7f0b031010
coffee <1219774571@qq.com>
2017-10-03 09:22:40 +0800

    getline

--- c_program/cat.c
+++ c_program/cat.c
@@ -6,6 +6,11 @@ int main(int argc,char *argv[])
 	int c;
 	if(fp = fopen(*++argv,"a+"))
 	{
+		if(argc != 2)
+		{
+			puts("格式错误,格式为cat file");
+			return -1;
+		}
 		while( (c = fgetc(fp)) && c != EOF)
 		{
 			printf("%c",c);
@@ -13,6 +18,7 @@ int main(int argc,char *argv[])
 	}
 	else
 	{
+		puts("文件打开失败.");
 		return 1;
 	}
 	fclose(fp);
