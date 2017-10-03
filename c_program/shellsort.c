1b88251256c72220fdd29a46c47639a9dd1b5de0
coffee <1219774571@qq.com>
2017-10-03 22:05:49 +0800

    shellsort

---
+++ c_program/shellsort.c
@@ -0,0 +1,22 @@
+
+// shell排序算法
+//控制两个被计较元素之间的距离
+//元素间移动位置
+//比较交换
+void shellsort(int v[],int n)
+{
+	int gap, i, j, temp;
+
+	for(gap = n/2; gap > 0; gap /=2)
+	{
+		for(i = gap; i < n; ++i)
+		{
+			for(j = i - gap; j >= 0 && v[j] > v[j+gap]; j -= gap)
+			{
+				temp = v[j];
+				v[j] = v[j+gap];
+				v[j+gap] = temp;
+			}
+		}
+	}
+}
